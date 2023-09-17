19ebe6c4f5b40633132f4444eb9c8d6848d7d4db
Danillo Bispo <danillo.bispo21@gmail.com>
2023-09-16 21:35:48 -0300

    Add retrieve video id parameter

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import express, { request } from "express"
 const app = express()
 app.use(cors())
 
-app.get("/summary", (request, response) => {
-  response.send("Hi!")
+app.get("/summary/:id", (request, response) => {
+  response.send("ID do vídeo" + request.params.id)
 })
 
 app.listen(3333, () => console.log("Server is running onm port 3333"))
